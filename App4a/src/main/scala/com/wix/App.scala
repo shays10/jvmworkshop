9c880f2cc363963c67228215ff882ffd86a41be6
liorwa <liorwa@wix.com>
2019-08-19 23:55:35 +0300

    new and improved app4

--- App4a/src/main/scala/com/wix/App.scala
+++ App4a/src/main/scala/com/wix/App.scala
@@ -1,15 +1,35 @@
 package com.wix
 
 import scala.io.Source
+import scala.util.Try
 
 /**
  * Hello world!
  *
  */
 object App  {
+
+  val LIKE_COL = 8
+
   def main(args: Array[String]) {
-    println(Source.fromResource("USVideos.csv").getLines().size)
-    Thread.sleep(100000000)
+    Thread.sleep(10000)
+    avgLikesForCountry("US")
+    avgLikesForCountry("FR")
+    avgLikesForCountry("GB")
+    Thread.sleep(10000)
+  }
+
+  private def avgLikesForCountry(code: String): Unit = {
+    val source = Source.fromResource(s"${code}videos.csv")
+
+    val likesSeq = source.getLines().map { line ⇒
+      val cols = line.split(",").map(_.trim)
+      val maybeLikesStr = cols.lift(LIKE_COL)
+      maybeLikesStr.flatMap(l ⇒ Try(l.toLong).toOption).getOrElse(0l)
+    }.toSeq
+
+    val avg = likesSeq.foldLeft(0L)(_ + _) / likesSeq.size
+    println(s"The Average likes for a video in the $code is $avg")
   }
 
 }
